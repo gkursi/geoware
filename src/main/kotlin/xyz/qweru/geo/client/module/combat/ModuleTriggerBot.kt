628d10a55437dff09411b456819dbd4dc2bb6c5f
qweru <itsqweru@proton.me>
2025-10-17 16:46:05 +0300

    feat: trigger bot ground ticks, auto tech

--- src/main/kotlin/xyz/qweru/geo/client/module/combat/ModuleTriggerBot.kt
+++ src/main/kotlin/xyz/qweru/geo/client/module/combat/ModuleTriggerBot.kt
@@ -1,23 +1,25 @@
 package xyz.qweru.geo.client.module.combat
 
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.hit.EntityHitResult
 import net.minecraft.util.hit.HitResult
-import net.minecraft.util.math.MathHelper
-import net.minecraft.util.math.Vec3d
 import org.lwjgl.glfw.GLFW
-import xyz.qweru.geo.client.event.PostTickEvent
+import xyz.qweru.geo.client.event.PreTickEvent
 import xyz.qweru.geo.client.module.move.ModuleSprint
 import xyz.qweru.geo.core.event.Handler
+import xyz.qweru.geo.core.manager.combat.Attack
+import xyz.qweru.geo.core.manager.combat.CombatState
+import xyz.qweru.geo.core.manager.combat.TargetTracker
 import xyz.qweru.geo.core.system.module.Category
 import xyz.qweru.geo.core.system.module.Module
-import xyz.qweru.geo.extend.getRelativeVelocity
 import xyz.qweru.geo.extend.target
 import xyz.qweru.geo.extend.thePlayer
 import xyz.qweru.geo.extend.theWorld
+import xyz.qweru.geo.helper.input.GameInput
 import xyz.qweru.geo.helper.player.AttackHelper
 import xyz.qweru.geo.helper.timing.TimerDelay
 import xyz.qweru.multirender.api.API
-import java.util.Random
+import java.util.*
 
 /**
  * TODO: move away from using simulated mouse clicks for attacking
@@ -25,41 +27,49 @@ import java.util.Random
 class ModuleTriggerBot : Module("TriggerBot", "Automatically hit entities when hovering them", Category.COMBAT) {
     val sGeneral = settings.group("General")
     val sFailAttack = settings.group("Fail Attack")
-    val sPickHit = settings.group("Pick Hit")
+    val sTech = settings.group("Auto Tech")
 
     val playerWeaponOnly by sGeneral.boolean("Player Weapon Only", "Only attack players with a weapon", true)
+    val attackFirst by sGeneral.boolean("Require Target", "Requires you to attack the player manually before tbotting", false)
     val delay by sGeneral.longRange("Delay", "Attack delay", 55L..100L, 0L..500L)
     val miss by sGeneral.float("Miss%", "Chance of missing an attack", 0.1f, 0f, 0.9f)
-    val awaitCrit by sGeneral.boolean("Await Crit", "Don't attack if a crit is possible", false)
-    val awaitJump by sGeneral.boolean("Await Jump", "Don't attack before the jump cooldown is over", true).visible { awaitCrit }
-    val itemCooldown by sGeneral.float("Cooldown", "Vanilla item cooldown required to attack", 0.9f, 0f, 1f)
+    val awaitCrit by sGeneral.boolean("Await Crit", "Don't attack if a crit will be possible", false)
+    val groundTicks by sGeneral.int("Ground Ticks", "Time to wait for crits after landing", 5, 0, 20)
+        .visible { awaitCrit }
+    val sprintCrit by sGeneral.boolean("Sprint Crit", "Don't reset sprint on crit", true)
+    val itemCooldown by sGeneral.float("Cooldown", "Vanilla item cooldown required to attack", 1f, 0f, 1f)
     val sprintReset by sGeneral.boolean("Sprint Reset", "Automatically resets sprint on hit", true)
 
-    val failAttack by sFailAttack.boolean("Fail Attack", "Try to attack (and fail) if the target is slightly out of reach", true)
+    val failAttack by sFailAttack.boolean("Fail Attack", "Try to attack (and fail) if the target is slightly out of reach", false)
     val failReach by sFailAttack.float("Fail Reach", "Extra reach for failing", 0.25f, 0.01f, 1f)
     val failChance by sFailAttack.float("Fail%", "Chance of failing an out-of-reach attack", 0.1f, 0f, 1f)
 
-    val pickHit by sPickHit.boolean("Pick Hit", "Hit enemies without fully recharging when they are almost out of reach", true)
-    val pickReach by sPickHit.float("Pick Reach%", "Min reach for doing a pick hit", 0.9f, 0.1f, 1f)
-    val pickCharge by sPickHit.float("Pick Charge", "Min pick charge", 0.3f, 0f, 1f)
-    val onlyBackwards by sPickHit.boolean("Only Backwards", "Only pick hit when you are moving backwards", true)
+    val critDeflect by sTech.boolean("Crit Deflect", "Sprinthit the opponent right after they land a crit on you", true)
+    val awaitDeflect by sTech.boolean("Await Deflect", "Don't hit until the crit has landed", true)
+        .visible { critDeflect }
+    val inputTime by sTech.longRange("Input Time", "How long to press W for, if you aren't already sprinting", 50L..100L, 0L..1000L)
+        .visible { critDeflect }
 
     val timer = TimerDelay()
     val random = Random()
+    var nextDamage = Attack()
+    var resetForward = false
+    var forwardTimer = TimerDelay()
 
     @Handler
-    private fun onTick(e: PostTickEvent) {
-        if (!inGame || mc.currentScreen != null || !timer.hasPassed()) return
+    private fun onTick(e: PreTickEvent) {
+        if (!inGame) return
+
+        if (resetForward) {
+            mc.options.forwardKey.isPressed = !forwardTimer.hasPassed() || GameInput.forwardKey
+            if (forwardTimer.hasPassed()) resetForward = false
+        }
+
+        if (mc.currentScreen != null || !timer.hasPassed())
+        nextDamage = Attack()
         if (mc.crosshairTarget is EntityHitResult) {
             val en = (mc.crosshairTarget as EntityHitResult).entity
-            val pickHit = canPickHit(mc.crosshairTarget!!.pos)
-
-            if (!pickHit && !AttackHelper.canAttack(en, playerWeaponOnly, cooldown = itemCooldown)) return
-            if (!mc.thePlayer.activeItem.isEmpty) return
-            if (!pickHit && awaitCrit && AttackHelper.willCrit(awaitJump = awaitJump) && !AttackHelper.canCrit()) return
-
-            if (sprintReset) ModuleSprint.reset()
-
+            if (en is PlayerEntity && checkPlayer(en)) return
             if (random.nextFloat() > miss) {
                 API.mouseHandler.press(GLFW.GLFW_MOUSE_BUTTON_1)
                 API.mouseHandler.release(GLFW.GLFW_MOUSE_BUTTON_1)
@@ -69,15 +79,40 @@ class ModuleTriggerBot : Module("TriggerBot", "Automatically hit entities when h
             if (random.nextFloat() > failChance) return
             val hit = mc.theWorld.target(mc.thePlayer.entityInteractionRange + failReach)
             if (hit !is EntityHitResult) return
-            
+
             API.mouseHandler.press(GLFW.GLFW_MOUSE_BUTTON_1)
             API.mouseHandler.release(GLFW.GLFW_MOUSE_BUTTON_1)
             timer.reset(delay)
         }
     }
 
-    fun canPickHit(hit: Vec3d): Boolean =
-        pickHit && !(onlyBackwards && mc.thePlayer.getRelativeVelocity().x >= 0f)
-                && mc.thePlayer.getAttackCooldownProgress(0.5f) >= pickCharge
-                && mc.thePlayer.eyePos.squaredDistanceTo(hit) >= MathHelper.square(pickReach * mc.thePlayer.entityInteractionRange)
+    fun checkPlayer(en: PlayerEntity): Boolean {
+        if (!mc.thePlayer.activeItem.isEmpty) return true
+        if (attackFirst && en != TargetTracker.target) return true
+
+        if (TargetTracker.target == null) TargetTracker.target = en
+        nextDamage = CombatState.TARGET.predictNextAttack()
+        val nextAttack = CombatState.SELF.predictNextAttack()
+        val sprinting = mc.thePlayer.isSprinting
+
+        if (!AttackHelper.canAttack(en, playerWeaponOnly, cooldown = itemCooldown)) return true
+        if (awaitCrit && AttackHelper.willCrit(groundTicks = groundTicks) && !nextAttack.crit) return true
+
+        if (critDeflect && !sprinting) {
+            if (CombatState.SELF.lastDamage.crit) {
+                if (!mc.options.forwardKey.isPressed) {
+                    GameInput.forwardKey = true
+                    resetForward = true
+                    forwardTimer.reset(inputTime)
+                }
+                ModuleSprint.sprint(true, now = true)
+            } else if (awaitDeflect && CombatState.TARGET.predictNextAttack().crit) {
+                return true
+            }
+        }
+        // this takes effect post tick
+        if (sprintReset && sprinting && !(nextAttack.crit && sprintCrit)) ModuleSprint.sprint(false)
+
+        return false
+    }
 }
